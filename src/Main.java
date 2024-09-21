d017e015a90e44122959e9a77781b2873e1a2fee
opgeho <opgeho@gmail.com>
2024-09-20 23:09:23 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        final double INTEREST_RATE = 0.17;
+
+        double initBalance = 5000;
+        double interest = 0;
+
+        interest = initBalance * INTEREST_RATE;
+        System.out.println("your interest for a balance of " + initBalance + "$ after one month is: " + interest);
+        initBalance += interest;
+        interest = initBalance * INTEREST_RATE;
+        System.out.println("your interest after two months is: " + interest);
+
+
+
     }
 }
